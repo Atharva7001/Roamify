35414fa9ec3d62c92e30498b0b4632440c71a8dc
unknown <atharvabhagwat1@gmail.com>
2025-04-23 16:51:35 +0530

    Commited by Atharva on 23/4/2025

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -10,6 +10,11 @@ export default function Sidebar() {
       <Logo />
       <AppNav />
       <Outlet />
+      <footer className={styles.footer}>
+        <p className={styles.copyright}>
+          &copy; Copyright {new Date().getFullYear()} by Roamify Inc.
+        </p>
+      </footer>
     </div>
   );
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,24 +1,40 @@
-import { useState } from "react";
 import styles from "./Login.module.css";
 import Button from "../components/Button";
 import PageNav from "../components/PageNav";
+import { useAuth } from "../contexts/FakeAuthContext";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
   // PRE-FILL FOR DEV PURPOSES
-  const [email, setEmail] = useState("jack@example.com");
+  const [email, setEmail] = useState("maddy@example.com");
   const [password, setPassword] = useState("qwerty");
+
+  const { isAuthenticated, login } = useAuth();
+  const navigate = useNavigate();
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (email && password) login(email, password);
+  }
+
+  useEffect(
+    function () {
+      if (isAuthenticated) navigate("/app", { replace: true });
+    },
+    [isAuthenticated]
+  );
 
   return (
     <main className={styles.login}>
       <PageNav />
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <div className={styles.row}>
           <label htmlFor="email">Email address</label>
           <input
             type="email"
             id="email"
-            onChange={(e) => setEmail(e.target.value)}
             value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
         </div>
 
@@ -27,8 +43,8 @@ export default function Login() {
           <input
             type="password"
             id="password"
-            onChange={(e) => setPassword(e.target.value)}
             value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
         </div>
 
